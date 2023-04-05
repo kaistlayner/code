7f88260d53b3589d046eb8276bef4fbf6e74d0a5
kaistlayner <layner@kaist.ac.kr>
2023-04-06 02:52:33 +0900

    readme(window cpp 실행) 추가

--- 1244.cpp
+++ 1244.cpp
@@ -1,26 +1,49 @@
 #include <iostream>
 #include <string>
+#include <algorithm>
 
 using namespace std;
 
 void copy(char *dst, char *src)
 {
-    // cout << "copy " << dst << ' ' << src << endl;
     int i;
     for (i = 0; src[i]; i++)
     {
         dst[i] = src[i];
     }
     dst[i] = '\0';
-    // cout << "result " << dst << endl;
 }
 
-void get_answer(char *cards, int chance, int len, char *ans)
+bool is_string_same(char* a, char* b, int len){
+    for (int i = 0; i < len; i++){
+        if(a[i] != b[i]){
+            return false;
+        }
+    }
+
+    return true;
+}
+
+void get_answer(char* cards, int chance, int len, char* ans, char* ideal, bool* done)
 {
     char buffer[7];
     // cout << "check: " << chance << " from " << cards << " current ans " << ans << endl;
-    if (chance == 0)
+    if (chance == 0 || *done){
         return;
+    }
+
+    if (is_string_same(cards, ideal, len)){
+        if(chance % 2 == 0){
+            *done = true;
+            copy(ans, ideal);
+            return;
+        }
+        else if(len > 1){
+            ans[len - 1] = cards[len - 2];
+            ans[len - 2] = cards[len - 1];
+        }
+        return;
+    }
 
     for (int i = 0; i < len - 1; i++)
     {
@@ -37,7 +60,7 @@ void get_answer(char *cards, int chance, int len, char *ans)
             }
             else
             {
-                get_answer(buffer, chance - 1, len, ans);
+                get_answer(buffer, chance - 1, len, ans, ideal, done);
             }
         }
     }
@@ -48,19 +71,27 @@ int main(int argc, char **argv)
     int test_case;
     int T;
 
-    // freopen("input.txt", "r", stdin);
+    freopen("input.txt", "r", stdin);
     cin >> T;
 
     for (test_case = 1; test_case <= T; ++test_case)
     {
-        char cards[7], ans[7];
+        char cards[7], ideal[7], ans[7];
         int chance, len = 0;
+        bool done = false;
         cin >> cards >> chance;
         copy(ans, cards);
 
-        for (; cards[len]; len++)
-            ;
-        get_answer(cards, chance, len, ans);
+        int arr[7];
+        for (; cards[len]; len++){
+            arr[len] = cards[len] - '0';
+        }
+        sort(arr, arr+len);
+        for (int i = 0; i < len; i++){
+            ideal[i] = (char)arr[i];
+        }
+
+        get_answer(cards, chance, len, ans, ideal, &done);
         cout << '#' << test_case << ' ' << ans << endl;
     }
     return 0;
--- README.md
+++ README.md
@@ -1,2 +1,8 @@
 # code
+
 code test
+
+c++ window 실행 환경
+
+1. https://sourceforge.net/projects/mingw/ 다운 후 c++ 다운
+2. https://codedragon.tistory.com/3260 환경변수 PATH에 mingw/bin 추가 (C:\mingw 로 생성됨)
