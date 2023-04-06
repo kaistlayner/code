5ddfd648f7e13209fb6a4dd8a83363e90f104362
MinjunCho <layner@kaist.ac.kr>
2023-04-06 17:40:24 +0900

    1245 pass

--- 1245.cpp
+++ 1245.cpp
@@ -1,13 +1,16 @@
 #include<iostream>
+#include<cmath>
 
 using namespace std;
+
+double ALLOW = 1.0e-12;
 
 int main(int argc, char** argv)
 {
 	int test_case;
 	int T;
 
-	//freopen("input.txt", "r", stdin);
+	freopen("input.txt", "r", stdin);
 	cin>>T;
 
 	for(test_case = 1; test_case <= T; ++test_case)
@@ -15,7 +18,7 @@ int main(int argc, char** argv)
 		int n, i;
 		cin >> n;
 
-		int x[10], w[10];
+		double x[10], w[10];
 		for(i=0; i<n; i++){
 			cin >> x[i];
 		}
@@ -23,9 +26,48 @@ int main(int argc, char** argv)
 			cin >> w[i];
 		}
 
+		cout << fixed;
+		cout.precision(10);
+		cout << "#" << test_case;
 
+		for(int i=0; i<n-1; i++){
+			double l = x[i], r = x[i+1];
 
-		cout << "#" << test_case << " " << 1 << endl;
+			while(1){
+				double sum = 0, last_sum = 0;
+				double point = (l + r) / 2;
+
+				for(int j=0; j<n; j++){
+					double force =  w[j] / pow(point - x[j], 2);
+					if(x[j] < point){
+						sum -= force;
+					}else{
+						sum += force;
+					}
+				}
+
+				if(last_sum && pow(last_sum, 2) < pow(sum, 2)){
+					break;
+				}
+
+				// cout << ' ' << i << ' ' << l << ' ' << point << ' '<< r <<  ' ' << sum << endl;
+	
+				if(sum == 0 || r - l < ALLOW){
+					cout << " " << point;
+					break;
+				}
+				else if(sum > 0){
+					r = point;
+				}
+				else{
+					l = point;
+				}
+
+				last_sum = sum;
+			}
+		}
+
+		cout << endl;
 	}
 	return 0;
 }
